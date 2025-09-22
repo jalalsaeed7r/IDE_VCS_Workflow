5f80f970f32912d956e0e2430e2671f8af8d7b89
ACJC One <ACJC One@DESKTOP-91FPAUE>
2025-09-21 22:10:02 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char first = 'M';
+    char middle = 'J';
+    char last = 'K';
+
+    cout << "Your initials are: " << first << middle << last << endl;
+    return 0;
+}
